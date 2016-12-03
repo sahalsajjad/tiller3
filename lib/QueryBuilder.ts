98e24c565a7572625d944c6b4ca16fcfaad1410e
wahtye <nico.ulbricht@live.de>
2016-12-03 16:22:33 +0100

    Querybuilder for One any Many

---
+++ lib/QueryBuilder.ts
@@ -0,0 +1,35 @@
+const generateLookupStage = (foreignCollection: string, localField: string, asField: string) => {
+    return {
+        $lookup: {
+            from: foreignCollection,
+            localField: localField,
+            foreignField: '_id',
+            as: asField
+        }
+    }
+}
+
+const localFieldToAs = (localField: string) => {
+    if (!localField.match(/_id$/)) {
+        throw new Error('localField is required to end in _id as per MongoDB standards')
+    }
+
+    return localField.replace('_id', '')
+}
+
+export class QueryBuilder {
+    static generateLookupMany(foreignCollection: string, localField: string): any[] {
+        const asField = localFieldToAs(localField)
+        return [generateLookupStage(foreignCollection, localField, asField)]
+    }
+
+    static generateLookupOne(foreignCollection: string, localField: string): any[] {
+        const asField = localFieldToAs(localField)
+        return [
+            generateLookupStage(foreignCollection, localField, asField),
+            {
+                $unwind: '$' + asField
+            }
+        ]
+    }
+}

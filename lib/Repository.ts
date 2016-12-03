9009f93f281e2c3fd1abc8bbd8c5eb647b19e357
Nico Ulbricht <wahtye@Nicos-MacBook-Pro.local>
2016-12-03 15:41:54 +0100

    Repository.findOne implementation

--- lib/Repository.ts
+++ lib/Repository.ts
@@ -29,8 +29,8 @@ export abstract class Repository<T extends Document> {
      * @param document      The document to create
      * @returns             The created document
      */
-    async insertOne(document: T):Promise<T> {
-        if(this.options.versionDocuments) {
+    async insertOne(document: T): Promise<T> {
+        if (this.options.versionDocuments) {
             document._version = document._version || 0
         }
 
@@ -47,8 +47,8 @@ export abstract class Repository<T extends Document> {
      *
      * @see insertOne
      */
-    async insertMany(documents: T[]):Promise<T[]> {
-        if(this.options.versionDocuments) {
+    async insertMany(documents: T[]): Promise<T[]> {
+        if (this.options.versionDocuments) {
             documents.forEach(d => d._version = d._version || 0)
         }
 
@@ -58,28 +58,45 @@ export abstract class Repository<T extends Document> {
 
     // TODO Make sure _version and _id are not in update or make sure it still works if its set to bad values ...
     async update(_id, _version: number, update: any): Promise<void> {
-        let r = await this.collection.updateOne({
-            _id: _id,
-            _version: _version
-        }, {
-            $set: update,
-            $inc: { _version: 1 },
-            $push: {
-                _log: update
+        let r = await this.collection.updateOne(
+            {
+                _id: _id,
+                _version: _version
+            },
+            {
+                $set: update,
+                $inc: { _version: 1 },
+                $push: {
+                    _log: update
+                }
             }
-        })
+        )
 
         if (r.modifiedCount != 1) {
             throw new Error('Attempted to update a stale or deleted object')
         }
     }
 
-    find(sel): Promise<T[]> {
-        return this.collection.find(sel).toArray()
+    /**
+     * Returns a MongoDB cursor for the given selector
+     * @param {any} sel
+     * @returns {Cursor}
+     */
+    cursor(sel: any): Cursor {
+        return this.collection.find(sel)
     }
 
-    cursor(sel): Cursor {
-        return this.collection.find(sel)
+    /**
+     * Executes given find selector
+     * @param {any} sel
+     * @returns {Promise<T[]>}
+     */
+    find(sel: any): Promise<T[]> {
+        return this.cursor(sel).toArray()
+    }
+
+    findOne(sel: any): Promise<T> {
+        return this.collection.findOne(sel)
     }
 }
 
